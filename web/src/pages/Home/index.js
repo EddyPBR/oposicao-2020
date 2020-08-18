cf71d6f5b3531a71f54b252e56fa909548c56d2f
EddyPBR <edvaldojunior1310@gmail.com>
2020-08-18 14:41:02 -0300

    Adicionado correçoes de responsividade para a sessao de posts recentes

--- web/src/pages/Home/index.js
+++ web/src/pages/Home/index.js
@@ -1,48 +1,51 @@
 import React from 'react';
+import candidate from '../../assets/images/RB2_2466.png';
 
 import './styles.css';
 
 function Index() {
   return (
-    <div className="recent-posts">
-      <div className="recent-post">
-        <div className="image">
+    <div className="home-page">
+      <div className="recent-posts">
+        <div className="recent-post">
+          <div className="image">
+          </div>
+          <div className="title">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </div>
+          <blockquote>por <span>Algum Candidato</span></blockquote>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+            commodo convallis lorem, efficitur aliquam quam posuere id. Quisque
+            vitae orci orci. In aliquam bibendum turpis vitae cursus
+          </p>
         </div>
-        <div className="title">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        <div className="recent-post">
+          <div className="image">
+          </div>
+          <div className="title">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </div>
+          <blockquote>por <span>Algum Candidato</span></blockquote>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+            commodo convallis lorem, efficitur aliquam quam posuere id. Quisque
+            vitae orci orci. In aliquam bibendum turpis vitae cursus
+          </p>
         </div>
-        <blockquote>por <span>Algum Candidato</span></blockquote>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-          commodo convallis lorem, efficitur aliquam quam posuere id. Quisque
-          vitae orci orci. In aliquam bibendum turpis vitae cursus
-        </p>
-      </div>
-      <div className="recent-post">
-        <div className="image">
+        <div className="recent-post">
+          <div className="image">
+          </div>
+          <div className="title">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </div>
+          <blockquote>por <span>Algum Candidato</span></blockquote>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+            commodo convallis lorem, efficitur aliquam quam posuere id. Quisque
+            vitae orci orci. In aliquam bibendum turpis vitae cursus
+          </p>
         </div>
-        <div className="title">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </div>
-        <blockquote>por <span>Algum Candidato</span></blockquote>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-          commodo convallis lorem, efficitur aliquam quam posuere id. Quisque
-          vitae orci orci. In aliquam bibendum turpis vitae cursus
-        </p>
-      </div>
-      <div className="recent-post">
-        <div className="image">
-        </div>
-        <div className="title">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </div>
-        <blockquote>por <span>Algum Candidato</span></blockquote>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-          commodo convallis lorem, efficitur aliquam quam posuere id. Quisque
-          vitae orci orci. In aliquam bibendum turpis vitae cursus
-        </p>
       </div>
     </div>
   );
