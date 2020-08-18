66c8102be394884dc53bbcad0ec0e0c7a8e3bac2
EddyPBR <edvaldojunior1310@gmail.com>
2020-08-18 13:45:33 -0300

    Adicionado sessao de posts recentes na pagina Home

--- web/src/pages/Home/index.js
+++ web/src/pages/Home/index.js
@@ -4,7 +4,47 @@ import './styles.css';
 
 function Index() {
   return (
-    <div />
+    <div className="recent-posts">
+      <div className="recent-post">
+        <div className="image">
+        </div>
+        <div className="title">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </div>
+        <blockquote>por <span>Algum Candidato</span></blockquote>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+          commodo convallis lorem, efficitur aliquam quam posuere id. Quisque
+          vitae orci orci. In aliquam bibendum turpis vitae cursus
+        </p>
+      </div>
+      <div className="recent-post">
+        <div className="image">
+        </div>
+        <div className="title">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </div>
+        <blockquote>por <span>Algum Candidato</span></blockquote>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+          commodo convallis lorem, efficitur aliquam quam posuere id. Quisque
+          vitae orci orci. In aliquam bibendum turpis vitae cursus
+        </p>
+      </div>
+      <div className="recent-post">
+        <div className="image">
+        </div>
+        <div className="title">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </div>
+        <blockquote>por <span>Algum Candidato</span></blockquote>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+          commodo convallis lorem, efficitur aliquam quam posuere id. Quisque
+          vitae orci orci. In aliquam bibendum turpis vitae cursus
+        </p>
+      </div>
+    </div>
   );
 }
 
