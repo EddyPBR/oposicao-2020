efd44457515ec9ebdbe8afb640d3a2038014af0c
ScriptRyan <ryanlima1309@gmail.com>
2020-09-10 06:21:52 -0300

    Alteraçoes na pagina de desenvolvimento

--- web/src/pages/Warning/index.js
+++ web/src/pages/Warning/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import teamwork from '../../assets/images/teamwork.jpg';
+// import teamwork from '../../assets/images/teamwork.jpg';
 
-import './styles.css';
+// import './styles.css';
 
 function Warning() {
   return (
     <div className="warning">
       <div className="container">
-        <div className="image">
+        {/* <div className="image">
           <img src={teamwork} alt="Imagem simbolizando trabalho em equipe" />
-          <a href='https://br.freepik.com/vetores/negocio'>Negócio vetor criado por pikisuperstar - br.freepik.com</a>
-        </div>
+        </div> */}
         <div className="content">
           <h1 className="title">Ainda estamos em desenvolvimento...</h1>
           <p>
