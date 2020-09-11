42c7110b63450278ace9a6034e5a9a17fd246323
Félix Vicente <felixvicent1306@gmail.com>
2020-09-11 13:18:02 -0300

    Add cors config and routes to images

--- server/public/index.php
+++ server/public/index.php
@@ -49,6 +49,9 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 |
 */
 
+header('Access-Control-Allow-Origin: *');  
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
--- server/routes/web.php
+++ server/routes/web.php
@@ -17,3 +17,39 @@ Route::get('/', 'HomeController@index');
 Route::get('/blog', 'HomeController@blog');
 Route::get('/post/{post}', 'HomeController@post');
 Route::get('/candidate/{post}', 'HomeController@candidate');
+
+Route::get('storage/posts/{filename}', function ($filename)
+{
+    $path = storage_path('app/public/posts/' . $filename);
+    
+
+    if (!File::exists($path)) {
+        abort(404);
+    }
+
+    $file = File::get($path);
+    $type = File::mimeType($path);
+
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", $type);
+
+    return $response;
+});
+
+Route::get('storage/candidates/{filename}', function ($filename)
+{
+    $path = storage_path('app/public/candidates/' . $filename);
+    
+
+    if (!File::exists($path)) {
+        abort(404);
+    }
+
+    $file = File::get($path);
+    $type = File::mimeType($path);
+
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", $type);
+
+    return $response;
+});
